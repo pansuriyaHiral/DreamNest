468e52581a2f3737be99cf07f6f493cc643dea84
PansuriyaHiral <pansuriyahiral845@gmail.com>
2025-03-15 10:54:59 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const bookingRoutes = require("./routes/booking.js")
 const userRoutes = require("./routes/user.js")
 
 app.use(cors({
-    origin: ["http://localhost:3001", "https://dream-nest-new.netlify.app"]
+    origin: ["http://localhost:3001"]
 }));
 app.use(express.json());
 app.use(express.static("public"));
