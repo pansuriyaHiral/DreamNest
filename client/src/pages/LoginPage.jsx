ffdf54d5b9bd030854aa07ed75952eaff7df0018
PansuriyaHiral <pansuriyahiral845@gmail.com>
2025-03-15 12:06:26 +0530

    commit

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -16,7 +16,7 @@ const LoginPage = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/auth/login`, {
+      const response = await fetch(`https://dream-nest-azure.vercel.app/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
